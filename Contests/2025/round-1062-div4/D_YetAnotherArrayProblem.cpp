f664198c4cbf7ffc9ba2b7da0283d4ecb8e2a90d
Diogo Porto <diogo.g.porto@gmail.com>
2025-10-29 18:13:11 +0000

    this suuuucks

--- Contests/2025/round-1062-div4/D_YetAnotherArrayProblem.cpp
+++ Contests/2025/round-1062-div4/D_YetAnotherArrayProblem.cpp
@@ -1,48 +1,36 @@
-#include <iostream>
-#include <numeric>
-//#include <bits/stdc++.h>
-#include <vector>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main(){
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
     int t;
     cin >> t;
-    while(t--){
+    while (t--) {
         int n;
         cin >> n;
-        vector<int> vec = {};
+        vector<long long> a(n);
+        for (int i = 0; i < n; ++i) cin >> a[i];
 
-        for(int i = 0; i < n; i++){
-            int x;
-            cin >> x;
-            vec.push_back(x);
-        }
+        // list of small primes to test
+        vector<int> primes = {
+            2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47,
+            53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107,
+            109, 113, 127, 131, 137, 139, 149, 151
+        };
 
-        if(vec[0] == 1){
-            cout << 2 << endl;
-            continue;
-        }
-
-
-        bool smallestExists = false;
-
-
-        for(int i = 2; i <= pow(10,18); i++){
-            for(int j = 0; j < n; j++){
-                if(__algo_gcd(i, vec[j]) == 1){
-                    smallestExists = true;
-                    cout << i << endl;
-                    break;
-                }
-            }
-            if(smallestExists == true){
-                break;
+        for (int p : primes) {
+            // check if there exists an element not divisible by p
+            if (any_of(a.begin(), a.end(), [p](long long x) { return x % p != 0; })) {
+                cout << p << '\n';
+                goto next_case; // go to next test case
             }
         }
-        if(!smallestExists){
-            cout << -1 << endl;
-        }
+
+        // if none of the primes worked
+        cout << -1 << '\n';
+
+        next_case:;
     }
-
-    return 0;
 }
